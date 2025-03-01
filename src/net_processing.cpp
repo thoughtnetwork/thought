a92e4a2f091808aa5d4514b4f03235d617121937
Phil Grim <phil@insufficient-light.com>
2025-02-28 20:21:16 -0500

    Reject connections from nodes older than 0.18.3.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1477,6 +1477,24 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             return false;
         }
 
+        // Hack for 0.18.4 because 0.18.3 should connect, but 0.18.2 and 0.18.1 should not.
+        // Won't need this going forward because we've increased the granularity of the PROTOCOL_VERSION
+        if (nVersion == nMinPeerProtoVersion)
+        {
+            LogPrintf("Checking for deprecated 0.18.x versions\n");
+            if (pfrom->cleanSubVer.find("0.18.2") != std::string::npos || 
+                pfrom->cleanSubVer.find("0.18.1") != std::string::npos ||
+                pfrom->cleanSubVer.find("0.18.0") != std::string::npos)
+            {
+                // disconnect
+                LogPrintf("peer=%d using deprecated version %s; disconnecting\n", pfrom->id, pfrom->cleanSubVer);
+                connman.PushMessage(pfrom, CNetMsgMaker(INIT_PROTO_VERSION).Make(NetMsgType::REJECT, strCommand, REJECT_OBSOLETE,
+                     strprintf("Version must be 0.18.3 or greater")));
+                pfrom->fDisconnect = true;
+                return false;
+            }
+        }
+
         if (nVersion == 10300)
             nVersion = 300;
         if (!vRecv.empty())
