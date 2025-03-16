3eec65e9d10f82fb457e1a13ee63b0abd43e13cb
Philip A Grim II <phil@insufficient-light.com>
2025-03-15 21:08:21 -0400

    Undoing unhelpful change.

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,30 +1,21 @@
 package=libevent
-$(package)_version=2.1.12-stable
-$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_version=2.1.12
+$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)-stable
+$(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
-$(package)_patches=cmake_fixups.patch
-$(package)_patches+=fix_mingw_link.patch
-$(package)_build_subdir=build
-
-# When building for Windows, we set _WIN32_WINNT to target the same Windows
-# version as we do in configure. Due to quirks in libevents build system, this
-# is also required to enable support for ipv6. See #19375.
-define $(package)_set_vars
-  $(package)_config_opts=-DEVENT__DISABLE_BENCHMARK=ON -DEVENT__DISABLE_OPENSSL=ON
-  $(package)_config_opts+=-DEVENT__DISABLE_SAMPLES=ON -DEVENT__DISABLE_REGRESS=ON
-  $(package)_config_opts+=-DEVENT__DISABLE_TESTS=ON -DEVENT__LIBRARY_TYPE=STATIC
-  $(package)_cppflags += -D_GNU_SOURCE
-  $(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601
-endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/cmake_fixups.patch && \
-  patch -p1 < $($(package)_patch_dir)/fix_mingw_link.patch
+  ./autogen.sh
+endef
+
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress
+  $(package)_config_opts_release=--disable-debug-mode
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) -S .. -B .
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
@@ -36,7 +27,6 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf bin && \
-  rm include/ev*.h && \
+endef
   rm include/event2/*_compat.h
 endef
